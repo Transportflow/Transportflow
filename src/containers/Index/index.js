876752ef73418fde13f4358d8426df300b7451ea
Adwirawien <hannsadrian@gmail.com>
2019-12-16 17:56:53 +0100

    Update description

--- src/containers/Index/index.js
+++ src/containers/Index/index.js
@@ -21,8 +21,8 @@ class App extends React.Component {
         return (
             <div className="p-6 pt-12 sm:p-20 lg:pl-56 bg-gray-400 dark\:bg-gray-800 min-h-screen trans">
                 <ImportantLine />
-                <h1 className="dark\:text-gray-200 font-bold font-inter text-2xl text-black">Transportflow</h1>
-                <p className="font-inter mb-5 text-gray-700">🚂🚟🚍🚊🚈🚀</p>
+                <h1 className="dark\:text-gray-200 font-bold font-inter text-2xl text-black">🦜 Transportflow</h1>
+                <p className="font-inter mb-5 text-gray-700 dark\:text-gray-400">Kein Auto, kein Problem. 🚀</p>
                 <div className="w-full sm:w-auto sm:max-w-xs mb-6">
                     <div className="mb-2 sm:mb-3">
                         <Link to="/monitor">
@@ -56,10 +56,10 @@ class App extends React.Component {
                             <div className="w-3/4">
                                 <p className="font-semibold text-xl flex items-center mt-1">
                                     <ion-icon name={"ios-navigate"} style={{fontSize: "24px"}}/>
-                                    <span className="truncate ml-1">Planner</span>
+                                    <span className="truncate ml-1">Planer</span>
                                 </p>
                                 <p className="font-normal truncate text-gray-700 dark\:text-gray-400">
-                                    Where do you want to go?
+                                    Routenplaner
                                 </p>
                             </div>
 
