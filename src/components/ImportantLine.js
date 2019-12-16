a88e04da0db58c99e05b47a34952f5485347dbd2
Adwirawien <hannsadrian@gmail.com>
2019-12-16 19:45:41 +0100

    Fix for some mobiles

--- src/components/ImportantLine.js
+++ src/components/ImportantLine.js
@@ -3,7 +3,7 @@ import DarkmodeToggle from "./DarkmodeToggle";
 import ImpressPrivacy from "./ImpressPrivacy";
 
 export default function ImportantLine() {
-    return (<span className={"flex italic dark:text-gray-500 text-gray-600 font-semibold"}>
+    return (<span className={"flex italic dark:text-gray-500 text-gray-600 font-semibold truncate"}>
                     <DarkmodeToggle/>
                     <span className="mx-1">|</span>
                     <ImpressPrivacy/>
