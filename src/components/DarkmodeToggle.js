15f04bc84f75f04937eeb3672e3b7f9c7615d20b
Adwirawien <hannsadrian@gmail.com>
2019-12-17 17:06:12 +0100

    Remove padding

--- src/components/DarkmodeToggle.js
+++ src/components/DarkmodeToggle.js
@@ -34,7 +34,7 @@ class DarkmodeToggle extends Component {
         return (
             <div>
                 {this.props.large ?
-                    <button className="focus:outline-none dark\:text-gray-400 text-gray-700 rounded-full p-3 bg-gray-800 dark\:bg-gray-400 trans" style={{width: "48px", height: "48px"}}
+                    <button className="focus:outline-none dark\:text-gray-400 text-gray-700 rounded-full bg-gray-800 dark\:bg-gray-400 trans" style={{width: "48px", height: "48px"}}
                             onClick={this.toggleDarkmode.bind(this)}>{this.state.darkmode === true ? "🌒" : "🌔"}</button>
                     :
                     <button className="italic focus:outline-none dark\:text-gray-400 text-gray-700"
