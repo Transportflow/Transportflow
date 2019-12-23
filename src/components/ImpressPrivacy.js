6dead8028cecf6d01db16fb9d54202f5710989cb
Adwirawien <hannsadrian@gmail.com>
2019-12-24 00:56:54 +0100

    Update Layout

--- src/components/ImpressPrivacy.js
+++ src/components/ImpressPrivacy.js
@@ -4,7 +4,7 @@ import {Link} from "react-router-dom";
 class ImpressPrivacy extends Component {
     render() {
         return (
-            <div className={"text-left absolute bottom-0 mb-2 flex w-full " + (this.props.centered ? "justify-center" : this.props.inline ? "w-auto mb-0 flex-none relative" : "")}>
+            <div className={"text-left absolute bottom-0 mb-4 flex w-full " + (this.props.centered ? "justify-center" : this.props.inline ? "w-auto mb-0 flex-none relative" : "")}>
                 <Link to={"/impressprivacy"}><button className="italic focus:outline-none dark\:text-gray-400 text-gray-700 mt-5">impressum & datenschutz</button></Link>
             </div>
         );
--- src/containers/Settings/settings.js
+++ src/containers/Settings/settings.js
@@ -6,6 +6,7 @@ import NetworkSwitch from "../../components/NetworkSwitch";
 import Checkmark from "../../components/Checkmark";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faDumpsterFire} from "@fortawesome/free-solid-svg-icons";
+import ImpressPrivacy from "../../components/ImpressPrivacy";
 
 class Settings extends Component {
     unregisterWorker() {
@@ -44,9 +45,10 @@ class Settings extends Component {
                     <p className="font-inter text-gray-700 dark\:text-gray-400 mb-2">Leeren Sie den Cache dieser Website.<br/>Dies aktualisiert Transportflow ggf. auf die neueste Version.</p>
                     <button
                         onClick={this.unregisterWorker}
-                        className={"text-gray-900 bg-gray-300 dark\\:bg-gray-700 dark\\:text-gray-300 dark-hover\\:bg-gray-600 sm:hover:bg-gray-300 px-5 py-3 rounded-lg text-base sm:hover:shadow-md focus:outline-none z-50 relative trans-fast " + this.props.className}>
+                        className="mb-20 text-gray-900 bg-gray-300 dark\\:bg-gray-700 dark\\:text-gray-300 dark-hover\\:bg-gray-600 sm:hover:bg-gray-300 px-5 py-3 rounded-lg text-base sm:hover:shadow-md focus:outline-none z-50 relative trans-fast">
                         <span className="mr-2"><FontAwesomeIcon icon={faDumpsterFire}/></span>Cache leeren
                     </button>
+                    <ImpressPrivacy/>
                 </div>
             </div>
         );
