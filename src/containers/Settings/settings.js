cb82d03e4a058983ea241c70a9f27a9c38384b40
Adwirawien <hannsadrian@gmail.com>
2019-12-23 23:42:18 +0100

    Add option to clear cache & update

--- src/containers/Settings/settings.js
+++ src/containers/Settings/settings.js
@@ -4,10 +4,13 @@ import DarkmodeToggle from "../../components/DarkmodeToggle";
 import BackButton from "../../components/BackButton";
 import NetworkSwitch from "../../components/NetworkSwitch";
 import Checkmark from "../../components/Checkmark";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faDumpsterFire} from "@fortawesome/free-solid-svg-icons";
 
 class Settings extends Component {
     unregisterWorker() {
         serviceWorker.unregister();
+        window.location.reload(true);
     }
 
     render() {
@@ -34,6 +37,16 @@ class Settings extends Component {
                     <p className="font-inter text-gray-700 dark\:text-gray-400 mb-2">Wählen Sie Ihr präferiertes
                         Design.</p>
                     <DarkmodeToggle button={true}/>
+
+                    <hr className="border-2 border-gray-500 rounded-lg dark\:border-gray-700 my-6"/>
+
+                    <h2 className="dark\:text-gray-200 font-semibold font-inter text-xl">Version</h2>
+                    <p className="font-inter text-gray-700 dark\:text-gray-400 mb-2">Leeren Sie den Cache dieser Website.<br/>Dies aktualisiert Transportflow ggf. auf die neueste Version.</p>
+                    <button
+                        onClick={this.unregisterWorker}
+                        className={"text-gray-900 bg-gray-300 dark\\:bg-gray-700 dark\\:text-gray-300 dark-hover\\:bg-gray-600 sm:hover:bg-gray-300 px-5 py-3 rounded-lg text-base sm:hover:shadow-md focus:outline-none z-50 relative trans-fast " + this.props.className}>
+                        <span className="mr-2"><FontAwesomeIcon icon={faDumpsterFire}/></span>Cache leeren
+                    </button>
                 </div>
             </div>
         );
