8324bba06d24147e6b38c88396b1eb5c4c4749d5
Adrian <hannsadrian@gmail.com>
2020-03-24 19:29:26 +0100

    Add settings

---
+++ src/views/Settings.svelte
@@ -0,0 +1,11 @@
+<script>
+    export let toggleDarkmode;
+    export let darkmode;
+</script>
+
+<main>
+    <button
+            class="my-auto focus:outline-none rounded-full bg-gray-800"
+            style="width: 48px; height: 48px"
+            on:click={toggleDarkmode()}>{darkmode ? "🌒" : "🌔"}</button>
+</main>
