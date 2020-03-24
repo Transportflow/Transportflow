ee2466c05c171a900fb1b39d15cbd3bc935c3f7d
Adrian <hannsadrian@gmail.com>
2020-03-24 19:29:38 +0100

    Add page animations

---
+++ src/utils/PageAnimator.svelte
@@ -0,0 +1,16 @@
+<script>
+    import {fly, fade} from "svelte/transition";
+    import {Router, Route} from "svelte-routing";
+
+    export let path;
+    export let prevUrl;
+</script>
+
+
+<Route {path}>
+    <div class="absolute w-full"
+         in:fly={prevUrl.length > path.length ? '{{ x: 2000, duration: 500 }}' : '{{ x: -500, duration: 500 }}'}
+         out:fly={prevUrl.length > path.length ? '{{ x: -500, duration: 500 }}' : '{{ x: 2000, duration: 500 }}'}>
+        <slot/>
+    </div>
+</Route>
