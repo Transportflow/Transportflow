6728f12bc792a6a25f1ae0fbc45c88d536139346
Adrian <hannsadrian@gmail.com>
2020-03-27 16:32:10 +0100

    Add nice primary Button

---
+++ src/components/PrimaryButton.svelte
@@ -0,0 +1,9 @@
+<script>
+    export let onClick;
+    export let text;
+</script>
+
+<button on:click={onClick}
+        class="px-4 py-2 rounded shadow transition-bg duration-200 font-medium text-sm text-gray-100 bg-button-blue hover:shadow-outline focus:outline-none active:bg-blue-700">
+    {text}
+</button>
