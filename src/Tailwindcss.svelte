8572dd813fc2ddc3dba9e5ede7cfb9a01fc17979
Adrian <hannsadrian@gmail.com>
2020-03-27 16:32:54 +0100

    Add blue bg color and blue outline shadow

--- src/Tailwindcss.svelte
+++ src/Tailwindcss.svelte
@@ -1,9 +1,17 @@
 <style global>
-  @tailwind base;
-  @tailwind components;
-  @tailwind utilities;
+    @tailwind base;
+    @tailwind components;
+    @tailwind utilities;
 
-  .parrot-green {
-    background: #85cb37
-  }
+    .parrot-green {
+        background: #85cb37
+    }
+
+    .bg-button-blue {
+        background: #1074e7;
+    }
+
+    .hover-shadow-outline-blue:hover {
+        box-shadow: 0 0 0 3px rgba(16, 116, 231, 1)
+    }
 </style>
