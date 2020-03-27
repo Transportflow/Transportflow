6b7cf585062720355fc6bb0c5bd56800534d5097
Adrian <hannsadrian@gmail.com>
2020-03-27 21:54:38 +0100

    Add BackButton

---
+++ src/components/BackButton.svelte
@@ -0,0 +1,9 @@
+<script>
+    import Button from "./Button.svelte";
+
+    function goBack() {
+        window.history.back();
+    }
+</script>
+
+<Button onClick={goBack} className="bg-gray-300 dark:bg-gray-700 dark-hover:bg-gray-900 hover:bg-gray-400 text-gray-700 dark:text-gray-400 shadow-none"><div class="flex"><ion-icon class="-ml-1 mr-1 my-auto text-gray-900 dark:text-gray-400" name="chevron-back-outline"></ion-icon> Zurück</div></Button>
