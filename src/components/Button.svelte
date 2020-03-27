9560a538d0ab5eeb5143f8b36d25502b8bfe0cef
Adrian <hannsadrian@gmail.com>
2020-03-27 21:54:44 +0100

    Add Button

---
+++ src/components/Button.svelte
@@ -0,0 +1,9 @@
+<script>
+    export let onClick;
+    export let className = "";
+</script>
+
+<button on:click={onClick}
+        class={"px-4 py-2 rounded shadow transition-bg duration-200 font-medium text-sm text-gray-100 focus:outline-none " + className}>
+    <slot/>
+</button>
