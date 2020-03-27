a454bd43d2a23895c81b63857e561bd57e41522c
Adrian <hannsadrian@gmail.com>
2020-03-27 21:55:21 +0100

    Use Button as foundation

--- src/components/PrimaryButton.svelte
+++ src/components/PrimaryButton.svelte
@@ -1,9 +1,7 @@
 <script>
+    import Button from "./Button.svelte";
     export let onClick;
     export let text;
 </script>
 
-<button on:click={onClick}
-        class="px-4 py-2 rounded shadow transition-bg duration-200 font-medium text-sm text-gray-100 bg-button-blue hover:shadow-outline focus:outline-none active:bg-blue-700">
-    {text}
-</button>
+<Button {onClick} className="bg-button-blue hover:shadow-outline active:bg-blue-700">{text}</Button>
