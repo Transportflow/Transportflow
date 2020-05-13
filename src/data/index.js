c80702eadd29c372a6a830ad9c4ce6800ce98037
Adrian <hannsadrian@gmail.com>
2020-05-13 17:32:56 +0200

    Update backend url

--- src/data/index.js
+++ src/data/index.js
@@ -1,7 +1,7 @@
 import {getRegions} from "./regions";
 
 import axios from "axios";
-axios.defaults.baseURL = "http://localhost:4567";
+axios.defaults.baseURL = "https://localhost:5678";
 
 function getAxios() {
     return axios;
