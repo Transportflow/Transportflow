a1cec017f11bccd8b20b56005bee668131eb1d09
Hanns Adrian Böhme <hannsadrian@gmail.com>
2020-09-05 10:48:14 +0200

    Improve Divider design

--- src/components/Divider.svelte
+++ src/components/Divider.svelte
@@ -2,4 +2,4 @@
     export let className = "";
 </script>
 
-<hr class={"rounded border-2 border-gray-300 dark:border-gray-700 transition-all duration-200 " + className}/>
+<hr class={"rounded border-2 bg-gray-300 border-gray-300 dark:bg-gray-700 dark:border-gray-700 transition-all duration-200 " + className}/>
