ae5f8e69dbb3de76d5f927450442af97a76d2394
Hanns Adrian Böhme <hannsadrian@gmail.com>
2020-09-06 09:32:55 +0200

    Make backend in deployed version configurable

--- src/data/index.js
+++ src/data/index.js
@@ -3,7 +3,7 @@ import {getStops, getNearbyStops, getDepartures, getUpcomingStops, getWagenreihu
 import {relativeTime, relativeToTime, clockTime} from "./time";
 
 import axios from "axios";
-axios.defaults.baseURL = process.env.BACKEND_URL;
+axios.defaults.baseURL = localStorage.getItem("baseURL") || process.env.BACKEND_URL;
 
 export let REGION_NOT_AVAILABLE = "<b>Region nicht verfügbar</b><br/>Bitte wählen Sie Ihre Region in den Einstellungen aus.";
 export let NETWORK_ERROR = "<b>Netzwerkfehler</b><br/>Vergewissern Sie sich, dass ihr Gerät mit dem Internet verbunden ist.";
