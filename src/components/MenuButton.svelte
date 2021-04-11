dd13e55f8fd5fe8d1de9fc3b9d265c8bde8e83ff
Adrian <hannsadrian@gmail.com>
2021-04-11 13:49:09 +0200

    Update MenuButton.svelte

--- src/components/MenuButton.svelte
+++ src/components/MenuButton.svelte
@@ -5,7 +5,7 @@
     export let onClick = () => {};
 </script>
 
-<button on:click={onClick} class="w-full p-2 pl-3 transition-bg duration-200 dark:text-gray-200 bg-gray-100 dark:bg-gray-700 dark-hover:bg-gray-900 hover:bg-gray-300 dark-active:bg-black focus:outline-none active:bg-gray-400">
+<button on:click={onClick} class="w-full p-2 pl-3 transition-bg duration-200 dark:text-gray-200 bg-white dark:bg-gray-700 dark-hover:bg-gray-900 hover:bg-gray-300 dark-active:bg-black focus:outline-none active:bg-gray-400">
     <div class="flex items-center">
         <ion-icon style="font-size: 24px" name={icon}></ion-icon>
         <h2 class={"text-xl font-semibold ml-1" + (icon === "bus" ? " mt-1" : "")}>{name}</h2>
